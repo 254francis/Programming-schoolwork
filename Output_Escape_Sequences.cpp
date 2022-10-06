2c6e26a4b217fd670884512d4e171de7bef0f8f1
254francis <kukufrank38@gmail.com>
2022-10-06 18:52:09 +0300

    first commit

---
+++ Output_Escape_Sequences.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+ int main()
+ {
+    cout <<"Today's class is online via Zoom\n\n";
+    cout <<"Dear DBIT Class\n";
+    cout <<"It's end-year already!";
+    return 0;
+ }
